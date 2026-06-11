6481e356df55ec31aa149c9e9fdad5f326a4b5db
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-06-11 10:30:00 +0100

    create all struture of db and create connection with testing it

--- config/database.php
+++ config/database.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Config;
 
 use PDO;
@@ -6,8 +8,8 @@ use PDOException;
 
 class Database {
     public static function getConnection(): PDO {
-        $host = '127.0.0.1';
-        $db   = 'pharmafefo';
+        $host = 'localhost';
+        $db   = 'pharma_fefo';
         $user = 'root';
         $pass = ''; 
 
@@ -16,8 +18,11 @@ class Database {
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
+            echo"connected with successfully";
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
         }
     }
 }
+// $result=new Database();
+// echo $result->getConnection();
