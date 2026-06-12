a90cdcd43976efb8866124fd48429a8460f2438b
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-06-12 12:00:00 +0100

    update database name

--- config/database.php
+++ config/database.php
@@ -18,11 +18,8 @@ class Database {
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
-            echo"connected with successfully";
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
         }
     }
 }
-// $result=new Database();
-// echo $result->getConnection();
